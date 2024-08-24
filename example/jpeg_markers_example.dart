ef2442f42f14acc626293cf303d0bbb9438833ff
Mgen <m@mgenware.com>
2024-08-25 02:52:45 +0800

    Update example

--- example/jpeg_markers_example.dart
+++ example/jpeg_markers_example.dart
@@ -4,12 +4,7 @@ import 'dart:io';
 import 'package:jpeg_markers/jpeg_markers.dart';
 
 Future<void> main(List<String> args) async {
-  args = ['test/files/pixel_8.jpg'];
-  if (args.isEmpty) {
-    return;
-  }
-
-  final file = File(args[0]);
+  final file = File('test/files/pixel_8.jpg');
   final bytes = await file.readAsBytes();
   scanJpegMarkers(bytes, (marker, offset) {
     if (marker.type == 0xE1) {
