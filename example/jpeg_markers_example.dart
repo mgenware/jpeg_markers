83bd4023d10b764b8a08509173bc1712f8e209cf
Mgen <m@mgenware.com>
2024-08-25 02:53:22 +0800

    Update example

--- example/jpeg_markers_example.dart
+++ example/jpeg_markers_example.dart
@@ -7,9 +7,6 @@ Future<void> main(List<String> args) async {
   final file = File('test/files/pixel_8.jpg');
   final bytes = await file.readAsBytes();
   scanJpegMarkers(bytes, (marker, offset) {
-    if (marker.type == 0xE1) {
-      print('  >>> e1 ');
-    }
     print('$offset: $marker');
     return true;
   });
