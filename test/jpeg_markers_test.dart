8d6b3b3fcd8199d42f1687ad8939cc6cf982c719
Mgen <m@mgenware.com>
2024-08-25 01:38:07 +0800

    Update tests

--- test/jpeg_markers_test.dart
+++ test/jpeg_markers_test.dart
@@ -4,20 +4,27 @@ import 'package:jpeg_markers/jpeg_markers.dart';
 import 'package:test/test.dart';
 
 Future<void> _t(String fileName) async {
-  final image = 'test/files/$fileName.jpg';
-  final dump = 'test/files/$fileName.txt';
+  final testFile = 'test/files/$fileName.jpg';
+  final dumpFile = 'test/files/$fileName.txt';
   final List<String> res = [];
-  scanJpegMarkers(await File(image).readAsBytes(), (offset, marker) {
+  scanJpegMarkers(await File(testFile).readAsBytes(), (offset, marker) {
     res.add('$offset: $marker');
     return true;
   });
   final actual = res.join('\n');
-  final expected = await File(dump).readAsString();
+
+  // Use this to update the expected file.
+  // await File(dumpFile).writeAsString(actual);
+
+  final expected = await File(dumpFile).readAsString();
   expect(actual, expected);
 }
 
 void main() {
   test('Base', () async {
     await _t('base');
+  });
+  test('Pixel 8', () async {
+    await _t('pixel_8');
   });
 }
